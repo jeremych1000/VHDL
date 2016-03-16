98a984c94b4ccac39fa6c068749aa268453599df
Jeremy Chan <jeremych@outlook.com>
2016-03-16 14:19:25 +0000

    a

--- cordic/combined_func_mult_inst.vhd
+++
@@ -1,6 +0,0 @@
-combined_func_mult_inst : combined_func_mult PORT MAP (
-		clock	 => clock_sig,
-		dataa	 => dataa_sig,
-		datab	 => datab_sig,
-		result	 => result_sig
-	);
--- cordic/test_cos_wrapper.do
+++
@@ -1,6 +0,0 @@
-restart -force -nowave;
-add wave -r /*;
-force clk 1 0 ns, 0 10ns -repeat 20ns;
-force radian 32'h0x00000000 20ns;
-run 1000ns;
-radix hexadecimal;
